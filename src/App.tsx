b29c52f9160cccb4ebaca14b5b8cc2f3c2e7a95c
usamecoban <usamecoban@gmail.com>
2025-02-14 00:40:28 +0300

    feat: update app structure and layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Layout } from "./components/Layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Layout } from "./components/Layout";
 import { ThemeProvider } from "./context/ThemeProvider";
+import { Flights } from "./pages/Flights";
 
 const queryClient = new QueryClient();
 
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <Layout>Test</Layout>
+        <Layout>
+          <Flights />
+        </Layout>
       </ThemeProvider>
     </QueryClientProvider>
   );
