4a59ba50a2690f531468ce9bc5d38813e0210417
U. Coban <ucoban.dev@gmail.com>
2025-02-15 03:38:57 +0300

    Feature: Add New Components for Flight Listing (#5)
    * refactor: enhance FlightSearch and Hero components
    
    * feat: add new components for flight listing

---
+++ src/components/FlightSearchWrapper.tsx
@@ -0,0 +1,17 @@
+import type { Itinerary } from "@/types/sky-scrapper";
+import { useState } from "react";
+import { FlightSearch } from "./FlightSearch";
+import FlightListing from "./FlightListing";
+
+const FlightSearchWrapper = () => {
+  const [flights, setFlights] = useState<Itinerary[] | undefined>([]);
+
+  return (
+    <div className="flex flex-col gap-16">
+      <FlightSearch setFlights={setFlights} />
+      <FlightListing flights={flights} />
+    </div>
+  );
+};
+
+export default FlightSearchWrapper;
